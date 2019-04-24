c678a1aeb9ba1e38d2912d981bd52e8c6c7aba60
mohsen <mohsen1993asgari@gmail.com>
2019-04-24 12:55:54 +0430

    Adds initializeTreeSet Method

--- src/tamrin2/Tamrin2.java
+++ src/tamrin2/Tamrin2.java
@@ -1,4 +1,17 @@
 package tamrin2;
 
+import java.util.Random;
+import java.util.TreeSet;
+
 public class Tamrin2 {
+
+    private static void initializeTreeSet(TreeSet<Character> firstSet, TreeSet<Character> secondSet) {
+        Random random = new Random();
+        while (firstSet.size() != 10)
+            firstSet.add((char) (random.nextInt(26) + 97));
+
+        while (secondSet.size() != 10)
+            secondSet.add((char) (random.nextInt(26) + 97));
+
+    }
 }
