819b9122d081a0c0b7f48ff0db7effd3d4f6d547
mohsen <mohsen1993asgari@gmail.com>
2019-04-27 11:36:53 +0430

    Adds initMap Method

--- src/tamrin2/Permute.java
+++ src/tamrin2/Permute.java
@@ -1,4 +1,21 @@
 package tamrin2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Permute {
+
+    private static Map<Character, Integer> initMap(String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int counter = 0;
+            for (int j = 0; j < word.length(); j++) {
+                if (ch == word.charAt(j))
+                    counter++;
+            }
+            map.put(ch, counter);
+        }
+        return map;
+    }
 }
